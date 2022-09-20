8072258ab3588b0d9438586837926d3a583ef25d
Muhammad Asif <alexasifhhossain@gmail.com>
2022-09-20 19:40:16 +0600

    header file dynamic title tag added

--- header.php
+++ header.php
@@ -1,3 +1,63 @@
+<?php
+include 'config.php';
+// Dynamic title code
+// echo "<pre>";
+// print_r($_SERVER);
+// echo "</pre>";
+// basename(): built in function gives the proper name of any file
+// example: news-blog-site/index.php convert to only 'index.php'
+$fileName = basename($_SERVER['PHP_SELF']);
+// echo $fileName;
+$pageTitle = '';
+
+switch ($fileName) {
+    case "category.php":
+        if(isset($_GET['cat_id'])){
+            $cat_id = $_GET['cat_id'];
+            $sql = "SELECT * FROM category WHERE category_id = $cat_id";
+            $result = mysqli_query($con, $sql) or die("Category Title Query Failed");
+            $row = mysqli_fetch_assoc($result);
+            $pageTitle = $row['category_name']. ' News';
+        } else {
+            echo "News Not Found";
+        }
+      break;
+    case "author.php":
+      if(isset($_GET['auth_id'])){
+        $auth_id = $_GET['auth_id'];
+        $sql = "SELECT * FROM user WHERE user_id = $auth_id";
+        $result = mysqli_query($con, $sql) or die("Author Title Query Failed");
+        $row = mysqli_fetch_assoc($result);
+        $pageTitle = $row['first_name']. ' '.$row['last_name']. ' News';
+      } else {
+        echo "News Not Found";
+      }
+      break;
+    case "single.php":
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            $sql = "SELECT * FROM post WHERE post_id = $id";
+            $result = mysqli_query($con, $sql) or die("Single Title Query Failed");
+            $row = mysqli_fetch_assoc($result);
+            $pageTitle = $row['title'];
+          } else {
+            echo "News Not Found";
+          }
+      break;
+    case "search.php":
+        if(isset($_GET['search'])){
+            $search = $_GET['search'];
+            $pageTitle = "Search: ".$search;
+          } else {
+            echo "News Not Found";
+          }
+      break;
+    default:
+      $pageTitle = 'News Site';
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +65,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>News</title>
+    <title><?php echo $pageTitle ?></title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <!-- Font Awesome Icon -->
